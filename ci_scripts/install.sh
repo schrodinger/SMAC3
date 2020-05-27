0ac421ca04139eff1e427b7b2963befd94c5087c
Ashwin Raaghav <ashraaghav@gmail.com>
2020-05-27 18:24:42 +0200

    pypy fails with sklearn

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -6,9 +6,9 @@ if [[ "$PYTHON_VERSION" == "pypy_conda" ]]; then
     pip install pep8 codecov flake8 pytest-cov
     # no-cache-dir for pyrfr because wheel needs to be built with a newer swig
     pypy -m pip install --no-cache-dir pyrfr>=0.8.0
-    # numpy conda install because BLAS missing in travis
-    conda install -y -c conda-forge blas
     conda install -y numpy scipy
+    # NOTE: scikit-learn does not work with conda yet (as of 2020-05-27)
+    conda install -y scikit-learn
     # install remaining packages with pypy
     cat requirements.txt | xargs -n 1 -L 1 pypy -m pip install
 
